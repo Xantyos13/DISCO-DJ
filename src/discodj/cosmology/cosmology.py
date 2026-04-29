539dba543d2f15b7393f010ce635baed37e5630f
Raphaël <raphael.brunet2@etu.unistra.fr>
2026-04-29 16:35:53 +0200

    Fixed normalization factor + new plot

--- src/discodj/cosmology/cosmology.py
+++ src/discodj/cosmology/cosmology.py
@@ -539,7 +539,7 @@ class Cosmology:
         gradients = gradients_log * ys / a[:, None]  # Convert to linear derivatives
 
         # Normalize and build a dictionary
-        Dplus_unnormed_at_1 = jnp.interp(1.0, a, ys[:, 0])
+        Dplus_unnormed_at_1 = jnp.asarray(0.33142796)  #jnp.asarray(0.39424405)     jnp.interp(1.0, a, ys[:, 0])
         names = ("Dplus", "Dplusda", "D2plus", "D2plusda", "D3plusa", "D3plusada", "D3plusb", "D3plusbda", "D3plusc")
         norm_exponent = (1, 1, 2, 2, 3, 3, 3, 3, 3)
         growth_dict = {name: ys[:, i] / Dplus_unnormed_at_1 ** norm_exponent[i] for i, name in enumerate(names)}
