6611ab1ae504f8218b39ee491de7db3f972008e1
Xantyos <xantyos@outlook.fr>
2026-05-18 11:00:46 +0200

    Code cleaning + new CI

--- src/discodj/cosmology/cosmology.py
+++ src/discodj/cosmology/cosmology.py
@@ -255,12 +255,9 @@ class Cosmology:
         return self.mnu * conKeV / Tnu0
     
     @property
-    def fr0(self):
-        """Initial Radiation over Matter fraction"""
-        if self.mnu == 0.0 : 
-            return (self.Omega_gamma +self.Omega_gamma * 3.046 * (7/8) * (4/11)**(4/3)) / (self.Omega_b + self.Omega_c ) # Case of the third neutrino is massless so radiation at a=1
-        else : 
-            return (self.Omega_gamma +self.Omega_gamma * 3.046 * (7/8) * (4/11)**(4/3)) / (self.Omega_b + self.Omega_c + self.Omega_nu_exact(1.0))  # Case of the third neutrino is massive so matter at a=1
+    def g_gamma_nu_0(self):
+        """Initial Radiation over Matter fraction""" 
+        return (self.Omega_gamma +self.Omega_gamma * 3.046 * (7/8) * (4/11)**(4/3)) / (self.Omega_b + self.Omega_c)
     
     # # # # # # # # # # # #
     # Jax PyTree methods
@@ -470,18 +467,18 @@ class Cosmology:
 
         ln_a = jnp.log(a)
 
-        y1_0 = jnp.log(a_min_integration)  # D1 ~ a
+        y1_0 = jnp.log(a_min_integration + (2/3)* self.g_gamma_nu_0)  # D1 ~ a +(2/3)*g_gamma_nu_0
         f1_0 = 1.0
-        D2_0 = -3.0 / 7.0 * a_min_integration ** 2  # D2 ~ -3/7 a^2
+        D2_0 = -3.0 / 7.0 * (a_min_integration + (2/3)*self.g_gamma_nu_0) ** 2  # D2 ~ -3/7 a^2
         y2_0 = jnp.log(jnp.abs(D2_0))
         f2_0 = 2.0
-        D3a_0 = 1.0 / 3.0 * a_min_integration ** 3  # D3a ~ +1/3 a^3
+        D3a_0 = 1.0 / 3.0 * (a_min_integration + (2/3)*self.g_gamma_nu_0) ** 3  # D3a ~ +1/3 a^3
         y3a_0 = jnp.log(jnp.abs(D3a_0))
         f3a_0 = 3.0
-        D3b_0 = -10.0 / 21.0 * a_min_integration ** 3  # D3b ~ -10/21 a^3
+        D3b_0 = -10.0 / 21.0 * (a_min_integration + (2/3)*self.g_gamma_nu_0) ** 3  # D3b ~ -10/21 a^3
         y3b_0 = jnp.log(jnp.abs(D3b_0))
         f3b_0 = 3.0
-        D3c_0 = 1.0 / 7.0 * a_min_integration ** 3  # D3c ~ +1/7 a^3
+        D3c_0 = 1.0 / 7.0 * (a_min_integration + (2/3)*self.g_gamma_nu_0) ** 3  # D3c ~ +1/7 a^3
         y3c_0 = jnp.log(jnp.abs(D3c_0))
         y0_log = jnp.array([y1_0, f1_0, y2_0, f2_0, y3a_0, f3a_0, y3b_0, f3b_0, y3c_0])
 
